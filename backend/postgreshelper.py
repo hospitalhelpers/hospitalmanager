784722a24302c9ea6ce9e4d0c8e90a38c0daa3d9
digitalyoshixi <digitalyoshixi@proton.me>
2025-08-02 13:53:25 -0400

    feat: new backend postgres file

---
+++ backend/postgreshelper.py
@@ -0,0 +1,23 @@
+import psycopg2
+# app.py
+from dotenv import load_dotenv
+import os
+load_dotenv()
+POSTGRESPASS = os.getenv('POSTGRESPASS')
+POSTGRESUSER = os.getenv('POSTGRESUSER')
+POSTGRESHOST = os.getenv('POSTGRESHOST')
+POSTGRESPORT = os.getenv('POSTGRESPORT')
+POSTGRESDBNAME = os.getenv('POSTGRESDBNAME')
+
+conn = psycopg2.connect(f"user={POSTGRESUSER} password={POSTGRESPASS} host={POSTGRESHOST} port={POSTGRESPORT} dbname={POSTGRESDBNAME}")
+
+def upload_patient_case():
+    with conn: # assuming we have connection
+        with conn.cursor() as dbcurs:
+            try:
+                dbcurs.execute("""
+                               """)
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+        
+    
