f181f0eb59937fba349bcbcc17f613740c148c44
digitalyoshixi <digitalyoshixi@proton.me>
2025-08-02 13:06:11 -0400

    chore: setup tailwindscss

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './tailwindstuff.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className='bg-white text-center'>
+    Hello world
+    </div>
   </StrictMode>,
 )
