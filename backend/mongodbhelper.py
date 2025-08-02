d0cbc7667006dd4ffd249a49bd7134e023a1448c
digitalyoshixi <digitalyoshixi@proton.me>
2025-08-02 18:23:53 -0400

    feat: adding apis for medicine into mongodb

--- backend/mongodbhelper.py
+++ backend/mongodbhelper.py
@@ -4,23 +4,41 @@ from dotenv import load_dotenv
 import certifi
 import os
 from Medicine import Medicine
+from bson import ObjectId
 
 ca = certifi.where()
 load_dotenv()
 ATLAS_URI = os.getenv('ATLAS_URI')
 client = MongoClient(ATLAS_URI, server_api=ServerApi('1'), tlsCAFile=ca)
+client.admin.command('ping')
+db = client["HealthApp"]
+print("Pinged your deployment. You successfully connected to MongoDB!")
 
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
 
-    medicine = Medicine(
-        medicationID="ibuprofen",
-        dosage="400mg", 
-        prescribedDate="2024-01-16"
-    )
 
-    client["HealthApp"]["Medications"].insert_one(medicine.model_dump())
+def add_medicine(medicationID : str = "somemedicine", dosage : str = "somedosage", prescribedDate : str = "somedate"):
+    try:
+        medicine = Medicine(
+            medicationID=medicationID,
+            dosage=dosage,
+            prescribedDate=prescribedDate
+        )
+        res = db["Medications"].insert_one(medicine.model_dump())
+        print("res: ", res.inserted_id)
+        return res.inserted_id
+    except Exception as e:
+        print(e)
+        return 0
 
-except Exception as e:
-    print(e)
+def get_medicine_from_id(id : str):
+    res = db["Medications"].find_one({"_id": ObjectId(id)})
+    returndict = {
+        "medicationID" : res["medicationID"],
+        "dosage" : res["dosage"],
+        "prescribedDate" : res["prescribedDate"],
+    }
+    return returndict
+
+def get_all_medicine():
+    res = db["Medications"].find()
+    return res
--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 import postgreshelper
+import mongodbhelper
 
 app = Flask(__name__) # designates this script as the root apth
 cors = CORS(app) # allow CORS for all domains on all routes.
@@ -20,6 +21,33 @@ def upload_userid():
             postgreshelper.upload_patient_case(request.json['userid'])
     # save this user id to database
     return "please use post"
+
+@app.route('/upload_medicine', methods = ['POST'])
+def upload_medicine():
+    if request.method == 'POST':
+        # parse the request as JSON
+        print(request.json)
+        if (request.json['medicationID']):
+            # upload userid to postgres
+            res = mongodbhelper.add_medicine(request.json['medicationID'], request.json['dosage'])
+            return str(res)  # Convert ObjectId to string
+    # save this user id to database
+    return "please use post"
+
+
+@app.route('/get_medicine', methods = ['GET'])
+def get_medicine():
+    if request.method == 'GET':
+        # parse the request as JSON
+        print(request.json)
+        if (request.json['id']):
+            # upload userid to postgres
+            res = mongodbhelper.get_medicine_from_id(request.json['id'])
+            print(res)
+            return res
+    # save this user id to database
+    return "please use post"
+
 
 if __name__ == "__main__": # if running this file directly
     app.run(debug=True, port=3000) # run the app
