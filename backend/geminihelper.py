72a290350b2ce20c5ab11f4985c69b982df4d683
Moiz Choudhary <moizc80@gmail.com>
2025-08-02 21:51:46 -0400

    Added langchain for google ai in requirements .txt

--- backend/geminihelper.py
+++ backend/geminihelper.py
@@ -8,5 +8,5 @@ GEMINI_API_KEY = os.getenv('GOOGLE_KEY')
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel('gemini-2.5-flash')
 
-response = model.generate_content("Hi, could you assess the care priority of someone with a history of asthma having severe flu symptoms? Return only an integer from 1 to 10")
+response = model.generate_content("Hi, could you assess the care priority of someone with a history of asthma having severe flu in a fast-paced hospital setting? Return only an integer from 1 to 1000")
 print(response.text)
